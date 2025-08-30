07d24561bd143a607fb51026e403c686a6eb8012
baxeezy <danya.bahv@yandex.ru>
2025-08-30 13:18:33 +0300

    Рефакторинг и создание AuthHelper

--- ImageFeed/Auth/WebViewPresenter.swift
+++ ImageFeed/Auth/WebViewPresenter.swift
@@ -1,9 +1,6 @@
 import UIKit
 
-enum WebViewConstants {
-    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
-}
-
+//MARK: - WebViewPresenterProtocol
 public protocol WebViewPresenterProtocol {
     func viewDidLoad()
     func didUpdateProgressValue(_ newValue: Double)
@@ -11,32 +8,25 @@ public protocol WebViewPresenterProtocol {
     var view: WebViewViewControllerProtocol? { get set }
 }
 
+//MARK: - WebViewPresenter
 final class WebViewPresenter: WebViewPresenterProtocol {
+
+    //MARK: - Properties
     weak var view: WebViewViewControllerProtocol?
+    var authHelper: AuthHelperProtocol
     
-    func viewDidLoad() {
-        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
-            return
-        }
-        
-        urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: Constants.accessKey),
-            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
-            URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: Constants.accessScope)
-        ]
-        
-        guard let url = urlComponents.url else {
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        
-        didUpdateProgressValue(0)
-        
-        view?.load(request: request)
+    init(authHelper: AuthHelperProtocol) {
+        self.authHelper = authHelper
     }
     
+    //MARK: - Public Methods
+    func viewDidLoad() {
+        guard let request  = authHelper.authRequest() else { return }
+        
+        didUpdateProgressValue(0)
+        view?.load(request: request)
+    }
+  
     func didUpdateProgressValue(_ newValue: Double) {
         let newProgressValue = Float(newValue)
         view?.setProgressValue(newProgressValue)
@@ -51,15 +41,6 @@ final class WebViewPresenter: WebViewPresenterProtocol {
     }
     
     func code(from url: URL) -> String? {
-        if
-            let urlComponents = URLComponents(string: url.absoluteString),
-            urlComponents.path == "oauth/authorize/native",
-            let items = urlComponents.queryItems,
-            let codeItem = items.first(where: { $0.name == "code"})
-        {
-            return codeItem.value
-        } else {
-            return nil
-        }
+        authHelper.code(from: url)
     }
 }
---
+++ ImageFeed/Constants/AuthConfiguration.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+enum Constants {
+    static let accessKey = "qzoF4exRpYsXJxjaBgLJFbbJY7Jo2KTdALw9KL2S3h8"
+    static let secretKey = "uoTrWlY9oJZM-92j1SsI_F1cp4wjb1ugUSEzxoSkRx0"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+
+}
+
+struct AuthConfiguration {
+    let accessKey: String
+    let secretKey: String
+    let redirectURI: String
+    let accessScope: String
+    let defaultBaseURL: URL
+    let authURLString: String
+    
+    init(accessKey: String, secretKey: String, redirectURI: String, accessScope: String, authURLString: String, defaultBaseURL: URL) {
+        self.accessKey = accessKey
+        self.secretKey = secretKey
+        self.redirectURI = redirectURI
+        self.accessScope = accessScope
+        self.defaultBaseURL = defaultBaseURL
+        self.authURLString = authURLString
+    }
+    
+    static var standard: AuthConfiguration {
+            return AuthConfiguration(accessKey: Constants.accessKey,
+                                     secretKey: Constants.secretKey,
+                                     redirectURI: Constants.redirectURI,
+                                     accessScope: Constants.accessScope,
+                                     authURLString: Constants.unsplashAuthorizeURLString,
+                                     defaultBaseURL: Constants.defaultBaseURL)
+        }
+}
+
+// Второй акк для запросов
+//enum Constants {
+//    static let accessKey = "fmlS9wi_GYdvMm3WNGCpnn1OnIV3fg8pSFhHFGy6uqw"
+//    static let secretKey = "jEuuMVrmDuz7fbUod4KtOF4tA27vFOxHlcd7frQmZlA"
+//    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+//    static let accessScope = "public+read_user+write_likes"
+//    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+//}
